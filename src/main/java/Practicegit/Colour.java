28de7ff0f016f21d183e75357e7f4a8919d3c4ed
DEEPOO KADAPA <DEEPOO KADAPA@LAPTOP-NHSCES8P>
2023-08-03 11:23:12 -0400

    this is Third commit

---
+++ src/main/java/Practicegit/Colour.java
@@ -0,0 +1,9 @@
+package Practicegit;
+
+public class Colour {
+	
+	public static void main(String[] args) {
+		System.out.println("Red colour");
+	}
+
+}
--- src/main/java/Practicegit/Sample.java
+++ src/main/java/Practicegit/Sample.java
@@ -17,5 +17,9 @@ public static void main(String[] args) {
 
 	
 	System.out.println("Hi this is my second commit");
+	
+	
+	
+	System.out.println("Hi this is my third commit");
 }
 }
