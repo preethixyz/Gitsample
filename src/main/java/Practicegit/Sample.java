cb8c7d65daf5819a96fa04d55660df501c6f3729
DEEPOO KADAPA <DEEPOO KADAPA@LAPTOP-NHSCES8P>
2023-08-03 10:43:16 -0400

    sample is committed 2

--- src/main/java/Practicegit/Sample.java
+++ src/main/java/Practicegit/Sample.java
@@ -12,7 +12,10 @@ public class Sample {
 public static void main(String[] args) {
 	
 	System.out.println("Hi this is my first commit");
+
+
+
+	
+	System.out.println("Hi this is my second commit");
 }
-
-
 }
