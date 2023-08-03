399a99b51730ef57617d5261a18ab71bba0892c8
PreethiSreerama <preethisreerama@gmail.com>
2023-08-03 10:32:57 -0400

    Sample is committed

---
+++ src/main/java/Practicegit/Sample.java
@@ -0,0 +1,18 @@
+package Practicegit;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+//@Test
+//	public void demo()
+//	{
+//		System.out.println("Hi this is my first commit");
+//	}
+
+public static void main(String[] args) {
+	
+	System.out.println("Hi this is my first commit");
+}
+
+
+}
